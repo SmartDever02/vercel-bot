9ff09d059804dbab1e6339dbb37af7e19697064b
iriswx <dreamboat0412@gmail.com>
2023-11-09 11:03:01 -0500

    C:/Program Files/Git/

--- app/sign-in/google-one-tap.tsx
+++ app/sign-in/google-one-tap.tsx
@@ -9,9 +9,7 @@ export default function GoogleOneTap() {
     <div
       id="g_id_onload"
       data-client_id={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string}
-      data-login_uri={`${
-        process.env.NEXT_PUBLIC_HOST_URL as string
-      }/api/auth/callback/google`}
+      data-login_uri={`api/auth/callback/google`}
       data-auto_select="true"
     ></div>
   )
