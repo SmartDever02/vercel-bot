4b758a02f7f0d7501dbd32237050fbb4b3ce6baa
SmartDever02 <smart.jamesjin@gmail.com>
2023-11-08 21:36:00 -0800

    redirect false

--- components/login-button.tsx
+++ components/login-button.tsx
@@ -61,7 +61,7 @@ export function GoogleLoginButton({
       onClick={() => {
         setIsLoading(true)
         // next-auth signIn() function doesn't work yet at Edge Runtime due to usage of BroadcastChannel
-        signIn('google', { callbackUrl: `/` })
+        signIn('google', { callbackUrl: `/`, redirect: false })
       }}
       disabled={isLoading}
       className={cn(className)}
